91846dd7836977bf86ff8b806cb23e20ad22b269
Mathew Carter <mathewacarter@gmail.com>
2025-09-07 19:42:01 -0400

    smart half width

--- src/components/Tubes/Tube/Tube.tsx
+++ src/components/Tubes/Tube/Tube.tsx
@@ -1,5 +1,6 @@
 import tubeImage from "../../../assets/tube.png";
 import classes from "./Tube.module.css";
+import { useMeasure } from "react-use";
 
 interface FilledTubeProps {
   tube: Tube;
@@ -12,12 +13,14 @@ const FilledTube = ({
   tube,
   onClick,
   isSelected,
-  isComplete
+  isComplete,
 }: FilledTubeProps) => {
+  const [ref, { width }] = useMeasure<HTMLDivElement>();
 
   const onClickTube = () => {
     onClick();
   };
+  console.log(width);
   return (
     <div
       className={classes.tube}
@@ -26,7 +29,7 @@ const FilledTube = ({
       <img
         src={tubeImage}
         alt="test tube"
-        style={{ height: "30vw", pointerEvents: "all", maxHeight: '300px' }}
+        style={{ height: "30vw", pointerEvents: "all", maxHeight: "300px" }}
         onClick={isComplete ? undefined : onClickTube}
       />
       {tube.balls.map((ball, index) => {
@@ -36,11 +39,13 @@ const FilledTube = ({
             : `${index * 20 + 10}%`;
         return (
           <div
+            ref={ref}
             key={index}
             className={classes.ball}
             style={{
               backgroundColor: ball.backgroundColor,
               bottom: bottomPercent,
+              left: `calc(50% - ${width / 2}px)`,
             }}
             onClick={isComplete ? undefined : onClickTube}
           ></div>
